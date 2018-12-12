dda7c99e0c3f253bf21ef7fc489a9eba8fd97c00
Alex Tucker <alex@floop.org.uk>
2018-12-12 18:07:40 +0000

    Monkeypatch messytables to catch exception looking for whether a table cell is bold. Fixes issue using savepreviewhtml().

--- gssutils/__init__.py
+++ gssutils/__init__.py
@@ -1,6 +1,28 @@
+import functools
+
+from messytables import excel
+from messytables.excel import XLSProperties
+
 from gssutils.scrape import Scraper
 from gssutils.utils import pathify, is_interactive
 from databaker.framework import *
 import pandas as pd
 from gssutils.metadata import Excel, ODS
 from pathlib import Path
+
+
+# Monkey patch an issue in Messytables that stops us from using savepreviewhtml from Databaker:
+
+
+def bold_wrapper(old_get_bold):
+    @functools.wraps(old_get_bold)
+    def new_get_bold(self):
+        try:
+            return old_get_bold(self)
+        except:
+            return False
+
+    return new_get_bold
+
+
+XLSProperties.get_bold = bold_wrapper(XLSProperties.get_bold)
