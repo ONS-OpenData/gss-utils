eb803351c37c5832c18c0d06f56f73b6b73bafb8
Alex Tucker <alex@floop.org.uk>
2019-07-12 10:15:26 +0100

    Really skip tests with @skip.

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 pip install -r test-requirements.txt
 pip install .
 patch -d /usr/local/lib/python3.7/site-packages/behave/formatter -p1 < cucumber-format.patch
-PYTHONDONTWRITEBYTECODE=1 behave -f json.cucumber -o test-results.json
+PYTHONDONTWRITEBYTECODE=1 behave --tags=-skip -f json.cucumber -o test-results.json
