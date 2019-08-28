28700e8132cb2350736d10e8708c3a823ae9924e
Alex Tucker <alex@floop.org.uk>
2019-08-28 14:08:07 +0100

    Another attempt to avoid writing __pycache__ directories.

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 pip install -r test-requirements.txt
 pip install .
 patch -d /usr/local/lib/python3.7/site-packages/behave/formatter -p1 < cucumber-format.patch
-PYTHONDONTWRITEBYTECODE=1 behave --tags=-skip -f json.cucumber -o test-results.json
+export PYTHONDONTWRITEBYTECODE=1
+behave --tags=-skip -f json.cucumber -o test-results.json
