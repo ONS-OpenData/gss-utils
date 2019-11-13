81b5692b16e109139aeafaa5f08cd0e359b5a379
RedWalters <jonathan.j.walters@Gmail.com>
2019-11-13 14:58:51 +0000

    TEMP fix for Issue #12
    PLEASE fix better than this when going back and looking at this, this is a crappy temp fix as to not block current work

--- gssutils/scrapers/govscot.py
+++ gssutils/scrapers/govscot.py
@@ -17,15 +17,17 @@ def scrape(scraper, tree):
     doctable = tree.xpath(
         "//table[contains(concat(' ', @class, ' '), ' dg file ')]")[0]
     for row in doctable.xpath('tr'):
-        if row.xpath('th/text()')[0].strip() == 'File:':
-            cell = row.xpath('td')[0]
-            dist = Distribution(scraper)
-            title_size_date = re.compile(r'(.*)\[([^,]+),\s+([0-9.]+)\s+([^:]+):\s([^\]]+)')
-            match = title_size_date.match(cell.text_content())
-            if match:
-                dist.title = match.group(1)
-                scraper.dataset.issued = parse(match.group(5)).date()
-            dist.downloadURL = urljoin(scraper.uri, cell.xpath('a/@href')[0])
-            dist.mediaType, encoding = mimetypes.guess_type(dist.downloadURL)
-            scraper.distributions.append(dist)
+        try:
+            if row.xpath('th/text()')[0].strip() == 'File:':
+                cell = row.xpath('td')[0]
+                dist = Distribution(scraper)
+                title_size_date = re.compile(r'(.*)\[([^,]+),\s+([0-9.]+)\s+([^:]+):\s([^\]]+)')
+                match = title_size_date.match(cell.text_content())
+                if match:
+                    dist.title = match.group(1)
+                    scraper.dataset.issued = parse(match.group(5)).date()
+                dist.downloadURL = urljoin(scraper.uri, cell.xpath('a/@href')[0])
+                dist.mediaType, encoding = mimetypes.guess_type(dist.downloadURL)
+                scraper.distributions.append(dist)
+        except:
             break
