b8eb4f82b7d2ab3c4be57016578e911548d6effa
Alex Tucker <alex@floop.org.uk>
2019-12-13 11:14:23 +0000

    Alternate Jenkins job to run scraper scenarios against live websites.

---
+++ test-scrapers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install -r test-requirements.txt
+pip install .
+patch -d /usr/local/lib/python3.7/site-packages/behave/formatter -p1 < cucumber-format.patch
+export PYTHONDONTWRITEBYTECODE=1
+behave -D record_mode=all --tags=-skip -f json.cucumber -o test-scraper-results.json features/scrape.feature
