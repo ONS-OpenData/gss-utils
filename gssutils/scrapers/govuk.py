d83d28d853e6c14afa999d31ed0dd88275afd763
Alex Tucker <alex@floop.org.uk>
2019-12-20 16:16:22 +0000

    gov.uk scraper now works for old scenarios. MHCLG Live Tables appear to have a different HTML layout.

--- gssutils/scrapers/govuk.py
+++ gssutils/scrapers/govuk.py
@@ -102,14 +102,26 @@ def scrape_collection(scraper, tree):
 
 
 def content_api(scraper, tree):
+    final_url = False
     uri_components = urlparse(scraper.uri)
-    metadata = scraper.session.get(f'https://www.gov.uk/api/content/{uri_components.path}').json()
-    schema = metadata['schema_name']
+    content_api_path = uri_components.path
+    while not final_url:
+        metadata = scraper.session.get(f'https://www.gov.uk/api/content/{content_api_path}').json()
+        schema = metadata['schema_name']
+        if schema == 'redirect':
+            if 'redirects' in metadata and len(metadata['redirects']) > 0:
+                content_api_path = metadata['redirects'][0]['destination']
+            else:
+                logging.error('Content API response is a redirect, but no redirection found.')
+        else:
+            final_url = True
     if schema == 'document_collection':
         content_api_collection(scraper, metadata)
     elif schema == 'publication':
         scraper.dataset = content_api_publication(scraper, metadata)
         scraper.distributions = scraper.dataset.distribution
+    elif schema == 'statistical_data_set':
+        content_api_sds(scraper, metadata)
     else:
         logging.warning(f'Unknown schema type {schema}')
 
@@ -195,3 +207,61 @@ def content_api_publication(scraper, metadata):
                             distributions.append(dist)
         ds.distribution = distributions
     return ds
+
+
+def content_api_sds(scraper, metadata):
+    # publications are in the details/body HTML
+    # they look to be a collection of datasets
+
+    if 'description' in metadata:
+        scraper.catalog.comment = metadata['description']
+    if 'first_published_at' in metadata:
+        scraper.catalog.issued = metadata['first_published_at']
+    if 'updated_at' in metadata:
+        scraper.catalog.modified = metadata['updated_at']
+    if 'links' in metadata and 'organisations' in metadata['links']:
+        orgs = metadata['links']['organisations']
+        if len(orgs) == 0:
+            logging.warning("No publishing organisations listed.")
+        elif len(orgs) >= 1:
+            if len(orgs) > 1:
+                logging.warning('More than one organisation listed, taking the first.')
+            scraper.catalog.publisher = orgs[0]["web_url"]
+    scraper.catalog.dataset = []
+    if 'details' in metadata and 'body' in metadata['details']:
+        body_tree = html.fromstring(metadata['details']['body'])
+        for heading in body_tree.xpath("//h2[following-sibling::p/descendant::span[@class='attachment-inline']]"):
+            id = heading.get('id')
+            ds = PMDDataset()
+            ds.title = heading.text
+            ds.comment = scraper.catalog.comment
+            ds.publisher = scraper.catalog.publisher
+            ds.issued = scraper.catalog.issued
+            ds.modified = scraper.catalog.modified
+            email_anchor = next(iter(body_tree.xpath("//a[@class='email']")), None)
+            if email_anchor is not None:
+                ds.contactPoint = email_anchor.get('href')
+            ds.distribution = []
+            for attachment in body_tree.xpath(f"//h2[@id='{id}']/" + \
+                                              f"following-sibling::p[preceding-sibling::h2[1][@id='{id}']]/" + \
+                                              "span[@class='attachment-inline']"):
+                dist = Distribution(scraper)
+                dist.title = next(iter(attachment.xpath("a/text()")), None)
+                dist.downloadURL = next(iter(attachment.xpath("a/@href")), None)
+                dist.mediaType, _ = mimetypes.guess_type(dist.downloadURL)
+                abbr = next(iter(attachment.xpath("descendant::abbr/text()")), None)
+                if abbr is not None:
+                    if abbr.upper() == 'PDF':
+                        dist.mediaType = PDF
+                    elif abbr.upper() == 'ODS':
+                        dist.mediaType = ODS
+                size = next(iter(attachment.xpath("descendant::span[@class='file-size']/text()")), None)
+                if size is not None:
+                    if size.endswith('KB'):
+                        dist.byteSize = int(float(size[:-2]) * 1024)
+                    elif size.endswith('kB'):
+                        dist.byteSize = int(float(size[:-2]) * 1000)
+                    elif size.endswith('MB'):
+                        dist.byteSize = int(float(size[:-2]) * 1000000)
+                ds.distribution.append(dist)
+            scraper.catalog.dataset.append(ds)
