3675a929f186b5796a47898202dd6635baee46fc
LPerryman <45283878+LPerryman@users.noreply.github.com>
2020-01-13 08:38:20 +0000

    Update refdata.py
    importing io

--- gssutils/refdata.py
+++ gssutils/refdata.py
@@ -1,7 +1,7 @@
 # ###### CODE TO CREATE A SET OF CODELISTS IN CSV FORMAT AS WELL AS COLUMN.CSV AND COMPONENTS.CSV FILES
 
-from gssutils.utils import *
-
+from gssutils.utils import pathify
+import io
 
 # +
 def create_ref_data(mainDat, paramDat, addPriority):
