b88afcb46f922256927d2de7011b1a3d46662725
Alex Tucker <alex@floop.org.uk>
2020-09-24 18:05:58 +0100

    Accommodate minor change to NHS Digital pages. Mark stuff todo.

--- gssutils/scrapers/nhs_digital.py
+++ gssutils/scrapers/nhs_digital.py
@@ -31,24 +31,30 @@ def scrape(scraper, tree):
             dataset.landingPage = urljoin(scraper.uri, href)
             article_tree = html.fromstring(scraper.session.get(dataset.landingPage).text)
             article_type = article_tree.xpath("//span[contains(concat(' ', @class, ' '), ' article-header__label ')]/text()")[0]
-            assert article_type == 'Publication', 'Expecting publication'
+            assert article_type.startswith('Publication'), 'Expecting publication'
             details_node = article_tree.xpath("//dl[@class='detail-list']")[0]
             details = {}
             for node in details_node:
                 if node.tag == 'dt' and node.get('class') == 'detail-list__key':
-                    key = node.text.strip()
+                    key = node.text.strip().lower()
+                    if key.endswith(':'):
+                        key = key[:-1].strip()
                 elif node.tag == 'dd' and node.get('class') == 'detail-list__value':
                     value = node.text.strip()
                     if key not in details:
                         details[key] = [value]
                     else:
                         details[key].append(value)
-            if 'Publication date:' in details:
-                dataset.issued = parse(details['Publication date:'][0], dayfirst=True)
+            if 'publication date' in details:
+                dataset.issued = parse(details['publication date'][0], dayfirst=True)
+            # Todo: spatiotemporal coverage and resolution/granularity
+            # Todo: national statistics / official statistics badges
             resources = article_tree.xpath("//ul[@data-uipath='ps.publication.resources-attachments']/li/a")
             for link in resources:
                 dist = Distribution(scraper)
                 dist.title = link.get('title')
+                if hasattr(dataset, 'issued'):
+                    dist.issued = dataset.issued
                 dist.downloadURL = urljoin(dataset.landingPage, link.get('href'))
                 file_data = link.xpath("div[@class='block-link__body']")[0]
                 dist.mediaType = str(file_data.xpath("meta/@content")[0])
