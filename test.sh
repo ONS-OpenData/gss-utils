8db6762d988e314562f9874784dd18eb6fa9010b
mikeAdamss <mikelovesbooks@gmail.com>
2020-09-30 16:54:29 +0100

    revert accidentally committed argument

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ pip install -r test-requirements.txt
 pip install .
 patch -d /usr/local/lib/python3.7/site-packages/behave/formatter -p1 < cucumber-format.patch
 export PYTHONDONTWRITEBYTECODE=1
-behave -D record_mode=none --tags=-skip -n 'deal with ONS publication datetime as Europe/London date.' -f json.cucumber -o test-results.json
+behave -D record_mode=none --tags=-skip -f json.cucumber -o test-results.json
